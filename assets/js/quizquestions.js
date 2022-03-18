68786635ea265e1a2ded44826d89c1d6490524ac
Sherreka Burton <rekadarnb@gmail.com>
2022-03-18 08:41:09 -0400

    added quiz questions

---
+++ assets/js/quizquestions.js
@@ -0,0 +1,73 @@
+//Questions taken from Assessment from Trilogy Bootcamps
+var quizQuestions = [
+{
+    title: "Inside the HTML document, where do you place your JavaScript code?",
+    choices: ["Inside the <head> element", "Inside the <script> element", "Inside the <link> element", "In the <footer> element"],
+    answer: "Inside the <script> element"
+},
+
+{
+    title: "What operator is used to assign a value to a declared variable?",
+    choices: ["Question mark (?)", "Colon (:)", "Equal sign (=)", "Double-equal (==)"],
+    answer: "Equal sign (=)"
+},
+
+{
+    title: "What are the six primitive data types in JavaScript?",
+    choices: ["string, num, falsy, bigInt, symbol, undefined", "sentence, int, truthy, bigInt, symbol, undefined", "string, number, boolean, bigInt, symbol, undefined", "sentence, float, data, bigInt, symbol, undefined"],
+    answer: "string, number, boolean, bigInt, symbol, undefined"
+},
+
+{
+    title: "How do we declare a conditional statement in JavaScript?",
+    choices: ["if...else", "difference...between", "for loop", "while loop"],
+    answer: "if...else"
+},
+
+{
+    title: "From the given array which index is the letter 'b' on? ['a', 'b', 'c', 'd']",
+    choices: ["0", "1", "2", "3"],
+    answer: "0"
+},
+
+{
+    title: "What are the two types of scope JavaScript uses?",
+    choices: ["Outside and Inside", "Abroad and Local", "Surrounding and Inner", "Global and Local"],
+    answer: "Global and Local"
+},
+
+{
+    title: "How do we access a value stored in an object?",
+    choices: ["Period notation, Square bracket notation", "Dot notation, Bracket notation", "Equal notation, Abstract notation", "Dot notation, Curl bracket notation"],
+    answer: "Dot notation, Bracket notation"
+},
+
+{
+    title: "What is an object method?",
+    choices: ["A function associated with an object", "A function that takes an object for an argument", "Keys in an object that have a number assigned to it", "An array saved inside of an object"],
+    answer: "A function associated with an object"
+},
+
+{
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+  },
+
+  {
+    title: "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  },
+  {
+    title: "What is the type of Pop up boxes available in JavaScript?:",
+    choices: ["Alert", "Confirm", "Prompt", "All the above"],
+    answer: "All the above"
+  }
+];
