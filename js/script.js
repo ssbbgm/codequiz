e23f12f51b958f0255b702597da1878cff8b5006
Sherreka Burton <rekadarnb@gmail.com>
2022-03-23 21:02:40 -0400

    basically restarted the code. Changed formatting in CSS and the HMTL structure. Removed an HTML page

--- js/script.js
+++ js/script.js
@@ -5,28 +5,30 @@ let startBtn = document.querySelector('#start');
 let questionsEl = document.querySelector('#questions');
 let choicesEl = document.querySelector('#choices');
 let initialSubmit = document.querySelector('#intials');
-let submitBtn = document.querySelector('submit');
+let submitBtn = document.querySelector('#submit');
 let feedbackEl = document.querySelector('#feedback');
 
 
 let totalTime = 200;
 let seconds = 0;
 
-function setCounterText() {
-    timeEl.textContent = time;
-  }
 
-startBtn.onclick = function startTime (){
 
-    var timeInterval = setInterval(function () {
-        //
-        // YOUR CODE HERE
-        //
-        counter++;
-        let seconds = totalTime - counter;
+// function setCounterText() {
+//     timeEl.textContent = time;
+//   }
+
+// startBtn.onclick = function startTime (){
+
+//     var timeInterval = setInterval(function () {
+//         //
+//         // YOUR CODE HERE
+//         //
+//         counter++;
+//         let seconds = totalTime - counter;
         
 
-      }, 1000);
-}
+//       }, 1000);
+// }
 
-setCounterText();
+// setCounterText();
